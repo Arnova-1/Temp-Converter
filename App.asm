8973e1bf4a5a68f5d40e49b04f6c2a1ffac0d888
Arnova-1 <arnova124@gmail.com>
2025-09-03 10:52:29 +0700

    Added parsing logic

--- App.asm
+++ App.asm
@@ -16,6 +16,27 @@ _start:
     mov rsi, buffer ; buffer address
     mov rdx, 64 ; max bytes
     syscall
+    ; rax = user input
+
+    ; begin parsing loop
+    ; initialize result = 0, index = 0
+    xor rbx, rbx ; result = 0 (rbx holds result)
+    xor rcx, rcx ; index = 0 (rcx holds i)
+
+parse_loop:
+    mov al, [buffer + rcx] ; load buffer[i]
+    cmp al, 10 ; compare with '\n' (ASCII 10)
+    je parse_done ; jump to parse_done if newline, stop loop
+
+    sub al, '0' ; convert char to digit by substracting '0'-'9' or ASCII 48 - 57 with '0' or ASCII 48
+    movzx rdx, al ; move digit into rdx (zero-extended)
+
+    mov rax, rbx ; copy result into rax
+    imul rbx, rbx, 10 ; multiply rbx by 10 -> shift digit to left
+    add rbx, rdx ; add the new digit
+
+    inc rcx ; i++
+    jmp parse_loop ; repeat
 
     mov rax, 60 ; syscall: exit
     xor rdi, rdi 
