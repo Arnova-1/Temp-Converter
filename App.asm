11cc1f4364b1e301651a317936765e4962a7407b
Arnova-1 <arnova124@gmail.com>
2025-09-03 08:34:19 +0700

    Enable user input via prompt

--- App.asm
+++ App.asm
@@ -1,17 +1,26 @@
 ; App.asm
-section .data
-msg db "1. Celcius to Fahrenheit", 10, "2. Fahrenheit to Celcius", 10, "Please select one of the operations: "
-msg_len equ $ - msg 
+section .bss
+buffer resb 64 ; Store input
 
 section .text
 global _start
 _start:
-    mov rax, 1
-    mov rdi, 1
+    mov rax, 1 ; syscall: write
+    mov rdi, 1 ; fd: stdout
     mov rsi, msg
     mov rdx, msg_len
     syscall
 
-    mov rax, 60
-    xor rdi, rdi
+    mov rax, 0 ; syscall: read
+    mov rdi, 0 ; fd: stdin
+    mov rsi, buffer ; buffer address
+    mov rdx, 64 ; max bytes
     syscall
+
+    mov rax, 60 ; syscall: exit
+    xor rdi, rdi 
+    syscall
+
+section .rodata
+msg db "1. Celcius to Fahrenheit", 10, "2. Fahrenheit to Celcius", 10, "Please select one of the following operations: "
+msg_len equ $ - msg 
